80893644fc0d56844a730e104ca57c399b2055c1
Dawid Wyrkowski (Dawqss) <dawidwyrkowski@gmail.com>
2024-08-19 12:27:15 +0200

    feat(): add SimpleXMLDebugger.ts

---
+++ app/utils/SimpleXMLDebugger.ts
@@ -0,0 +1,14 @@
+class SimpleXMLDebugger extends XMLHttpRequest {
+    constructor() {
+        super();
+    }
+
+    open(method: string, url: string, async?: boolean, user?: string | null, password?: string | null) {
+        console.log(`method: ${method}\n url: ${url}\n async: ${async}\n user: ${user}\n password: ${password}\n`);
+        super.open(method, url, async, user, password);
+    }
+}
+if (__DEV__) {
+    global.XMLHttpRequest = SimpleXMLDebugger;
+}
+
