6d3835f830856edc0863992286d36377d50dc06e
Dawid Wyrkowski (Dawqss) <dawidwyrkowski@gmail.com>
2024-09-26 21:42:15 +0200

    feat(): add base navigation

---
+++ app/screens/MapCityListScreen/MapCityListScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export function MapCityListScreen() {
+  return (
+    <View>
+      <Text>Konichiwa I am Map City List screen</Text>
+    </View>
+  );
+}
---
+++ app/screens/WeatherDetailsScreen/WeatherDetailsScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export function WeatherDetailsScreen() {
+  return (
+    <View>
+      <Text>Konichiwa I am Weather Detail Screen</Text>
+    </View>
+  );
+}
