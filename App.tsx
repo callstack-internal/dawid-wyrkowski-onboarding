51aae6738773cf7a83074d40307caa5faac3f6a7
Dawid Wyrkowski <Dawidwyrkowski@gmail.com>
2024-09-26 21:53:47 +0200

    Feat/add simple routing (#6)
    * feat(): install react-native-navigation
    
    * feat(): add base navigation

--- App.tsx
+++ App.tsx
@@ -1,51 +1,8 @@
-import React, { ReactElement, useEffect } from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  useColorScheme,
-  View
-} from 'react-native';
-
-import { Colors, Header } from 'react-native/Libraries/NewAppScreen';
-import WeatherApi from './app/http/WeatherApi.ts';
-import { getUniqueCities } from './app/http/utils.ts';
+import React, { ReactElement } from 'react';
+import { MainRouterNavigation } from './app/navigation/MainRouter.tsx';
 
 function App(): ReactElement {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const lat = 54.3667617;
-  const lng = 18.6999335;
-
-  useEffect(() => {
-    WeatherApi.findCitiesWeatherInRadiusByLatLng(lat, lng, 15, 100)
-      .then((arg) => console.log('here I am', getUniqueCities(arg.data.list)))
-      .catch((err) => console.log('here I am catching error babe', err));
-  }, []);
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}
-      >
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white
-          }}
-        />
-      </ScrollView>
-    </SafeAreaView>
-  );
+  return <MainRouterNavigation />;
 }
 
 export default App;
---
+++ app/navigation/MainRouter.tsx
@@ -0,0 +1,26 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createStaticNavigation } from '@react-navigation/native';
+import { MapCityListScreen } from '../screens/MapCityListScreen/MapCityListScreen.tsx';
+import { WeatherDetailsScreen } from '../screens/WeatherDetailsScreen/WeatherDetailsScreen.tsx';
+
+/**
+ * Add here types for the screens that are passed by navigation
+ */
+type RootStackParamList = {
+  MapCityList: {};
+  WeatherDetails: {};
+};
+
+const RootStack = createNativeStackNavigator<RootStackParamList>({
+  screens: {
+    MapCityList: {
+      screen: MapCityListScreen,
+      options: {
+        headerShown: false
+      }
+    },
+    WeatherDetails: WeatherDetailsScreen
+  }
+});
+
+export const MainRouterNavigation = createStaticNavigation(RootStack);
