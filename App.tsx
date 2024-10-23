23ab172d81e414562f63a640a93670b9495a1e11
Dawid Wyrkowski (Dawqss) <dawidwyrkowski@gmail.com>
2024-10-23 15:54:52 +0200

    chore(): trigger PR

--- App.tsx
+++ App.tsx
@@ -1,6 +1,10 @@
 import React, { ReactElement } from 'react';
 import { MainRouterNavigation } from './app/navigation/MainRouter.tsx';
 
+/**
+ * This is main app point
+ * @constructor
+ */
 function App(): ReactElement {
   return <MainRouterNavigation />;
 }
